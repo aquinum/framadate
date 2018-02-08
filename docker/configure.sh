1a2674a7fe251b39b3b68caa266096996429de9a
Arnaud de Mouhy <arnaud.demouhy@akerbis.com>
2018-02-08 17:43:09 +0100

    Better handling of composer in dev environment

--- docker/configure.sh
+++ docker/configure.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/html
 
-gosu www-data php composer.phar install
+gosu www-data php composer.phar install --no-dev --prefer-dist --classmap-authoritative
 gosu www-data mkdir -p tpl_c/
 
 cp -f admin/htaccess.txt admin/.htaccess
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,16 @@ set -eux
 
 htpasswd -nb ${ADMIN_USERNAME} ${ADMIN_PASSWORD} > /var/www/.htpasswd
 
+# Install composer dependencies if the vendor dir is missing
+# (likely during the first container startup in development)
+if [ ! -d "/var/www/html/vendor" ];
+then
+    # Install composer if missing
+    if [ ! -f "/var/www/html/composer.phar" ];
+    then
+        bash /tmp/setup/install_composer.sh
+    fi
+    gosu www-data php composer.phar install
+fi
+
 docker-php-entrypoint apache2-foreground
